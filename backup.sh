9a09dba7edf29d0dbf463a4eafb0effc839bcd9d
Valeria <valeria.casado@bbc.co.uk>
2025-01-14 15:22:10 +0000

    Store raw users, groups, policies and roles

---
+++ backup.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+### Start of validation ###
+
+# Check if AWS CLI version is 2.*
+aws_version=$(aws --version 2>&1)
+if [[ $aws_version != aws-cli/2* ]]; then
+    echo "Error: AWS CLI version 2 is required"
+    exit 0
+fi
+    
+# if $1 does not exist or it's help
+if [ -z "$1" ] || [ "$1" == "help" ]; then
+    echo "Missing positional argument: -a or -account [number]"
+    exit 0
+fi
+
+# Wrong args
+if ! [[ "$1" == "-a" ]] || [[ "$1" == "-account" ]]; then
+    echo "Only positional argument allowed: -a or -account [number]"
+    exit 0
+fi
+
+# Correct args, account info is wrong
+if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+    echo "The second argument needs to be an account number"
+    exit 0
+fi
+
+# Couldn't set credentials
+if [ "$AWS_ACCOUNT_ID" != "$2" ]; then
+    echo "To export IAM data, you need to set the AWS_ACCOUNT_ID environment variable to $2"
+    echo "Please run:"
+    echo "gobbc aws-credentials -account $2" 
+    exit 0
+fi
+
+### End of validation ###
+
+# Make sure we're not saving anything corrupted
+check_last_command() {
+    if [ $? -ne 0 ]; then
+        rm -rf "raw_$2"
+        echo "Error: Failed to execute the last command: $cmd"
+        exit 0
+    fi
+}
+
+# Commands to execute
+commands=(
+    "aws iam list-users --output yaml > raw_$2/users.yaml"
+    "aws iam list-groups --output yaml > raw_$2/groups.yaml"
+    "aws iam list-roles --output yaml > raw_$2/roles.yaml"
+    "aws iam list-policies --scope Local --output yaml > raw_$2/policies.yaml"
+)
+
+# Start saving data
+mkdir "raw_$2"
+for cmd in "${commands[@]}"; do
+    echo $cmd
+    eval $cmd
+    check_last_command
+done
+
+
---
+++ reformat_to_aws_access.py
@@ -0,0 +1,17 @@
+import sys
+
+# This script intends to grab all raw yaml downloads and transform them 
+# into something that can be easily put into aws-access
+
+def main():
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: reformat_to_aws_access.py <account_id>")
+        sys.exit(1)
+    
+    account_id = sys.argv[1]
+    print(f"Processing account ID: {account_id}")
+
+
+
+if __name__ == "__main__":
+    main()
